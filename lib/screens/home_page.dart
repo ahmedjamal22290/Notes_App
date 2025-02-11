7ef87331a1c0d699cd55f67d024a3e0b70536caa
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-02-11 13:40:37 +0200

    create app bar for home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,8 +6,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      // backgroundColor: Colors.white,
+      appBar: AppBar(
+        actions: [
+          Container(
+            height: 40,
+            margin: EdgeInsets.only(right: 8),
+            width: 45,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.white.withOpacity(0.15),
+            ),
+            child: Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+        ],
+        title: Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+      ),
     );
   }
 }
