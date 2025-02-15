7b1c1887c0375c9bc8a6adfd103ba023127328fd
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-02-15 12:50:23 +0200

    make bottom sheet scrollable

--- lib/widgets/add_note_bottomSheet.dart
+++ lib/widgets/add_note_bottomSheet.dart
@@ -8,62 +8,64 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Padding(
       padding:
           const EdgeInsets.only(top: 40.0, right: 20, left: 20, bottom: 40),
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelStyle: const TextStyle(color: Colors.deepPurpleAccent),
-              labelText: "Title",
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: Colors.pink,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.deepPurpleAccent),
+                labelText: "Title",
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(),
                 ),
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          TextField(
-            maxLines: 5,
-            decoration: InputDecoration(
-              labelStyle: const TextStyle(color: Colors.deepPurpleAccent),
-              labelText: "Content",
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: Colors.pink,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.pink,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                borderRadius: BorderRadius.circular(20),
               ),
             ),
-          ),
-          const Spacer(),
-          Container(
-            height: 45,
-            width: MediaQuery.of(context).size.width, //all width of screen
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.deepPurpleAccent,
+            const SizedBox(height: 20),
+            TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.deepPurpleAccent),
+                labelText: "Content",
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.pink,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
             ),
-            child: GestureDetector(
-              onTap: () {},
-              child: const Center(
-                child: Text(
-                  'Add',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
+            SizedBox(height: MediaQuery.of(context).size.height / 6),
+            Container(
+              height: 45,
+              width: MediaQuery.of(context).size.width, //all width of screen
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.deepPurpleAccent,
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Center(
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
